d715f6895ddac7cc167ec5fd761e167757cdafdb
Armruo <armruo@outlook.com>
2025-03-14 16:43:55 +0800

    change slogan

--- components/ui/mobile-menu.tsx
+++ components/ui/mobile-menu.tsx
@@ -114,7 +114,8 @@ export default function MobileMenu() {
               <li>
                 <Link
                   href="/docs"
-                  className="text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 px-5 py-2 flex items-center transition duration-150 ease-in-out"
+                  className="flex text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-gray-100 py-2"
+                  onClick={() => setMobileNavOpen(false)}
                 >
                   Docs
                 </Link>
