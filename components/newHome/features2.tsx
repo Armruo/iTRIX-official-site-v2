d32a56c227fb4bea02839e6b12bcef88b2d06faf
Armruo <armruo@outlook.com>
2025-04-03 17:53:31 +0800

    gradient

--- components/newHome/features2.tsx
+++ components/newHome/features2.tsx
@@ -14,7 +14,7 @@ const Features2 = ({}: RoadmapProps) => {
     const { t } = useTranslation();
     
     return (
-        <Section className="overflow-hidden">
+        <Section className="overflow-hidden relative">
             <div className="container md:pb-10">
                 <Heading tag="real-time influence anywhere anytime" title={t("common.features.title")} />
                 <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
@@ -75,7 +75,6 @@ const Features2 = ({}: RoadmapProps) => {
                                         </div>
                                     </div>
                                     <h4 className="h4 mb-4">{t(item.title)}</h4>
-                                    
                                     {item.items ? (
                                         <div className="space-y-4">
                                             {item.items.map((subItem, idx) => (
@@ -92,17 +91,19 @@ const Features2 = ({}: RoadmapProps) => {
                             </div>
                         </div>
                     ))}
-                    <div className="absolute top-[18.25rem] -left-[30.375rem] w-[56.625rem] opacity-60 mix-blend-color-dodge pointer-events-none">
-                        <div className="absolute top-1/2 left-1/2 w-[58.85rem] h-[58.85rem] -translate-x-3/4 -translate-y-1/2">
-                            <Image
-                                className="w-full"
-                                src="/images/new-home/gradient.png"
-                                width={942}
-                                height={942}
-                                alt="Gradient"
-                            />
-                        </div>
-                    </div>
+                </div>
+            </div>
+            {/* Gradient background */}
+            <div className="absolute inset-0 pointer-events-none">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                    <Image
+                        className="w-[58.85rem] opacity-60 mix-blend-color-dodge"
+                        src="/images/new-home/gradient.png"
+                        width={942}
+                        height={942}
+                        alt="Gradient"
+                        priority
+                    />
                 </div>
             </div>
         </Section>
